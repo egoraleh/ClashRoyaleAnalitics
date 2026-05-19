970ef3158ae9ca09cf7b9d6a278e2c131dc7df6e
petr <petrartemev05@gmail.com>
2026-05-19 17:58:02 +0300

    init

---
+++ clashroyaleback/src/test/java/egorteam/clashroyaleback/ClashroyalebackApplicationTests.java
@@ -0,0 +1,13 @@
+package egorteam.clashroyaleback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClashroyalebackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
