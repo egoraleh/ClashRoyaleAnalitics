39b1d0e3b03b1a45274657c1e5aea15392a6578d
petr <petrartemev05@gmail.com>
2026-05-27 14:10:04 +0300

    delete wrong directory

--- clashroyaleback/src/main/java/egorteam/clashroyaleback/persistence/repository/PublicDecksRepository.java
+++
@@ -1,7 +0,0 @@
-package egorteam.clashroyaleback.persistence.repository;
-
-import egorteam.clashroyaleback.persistence.entity.PublicDeckEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PublicDecksRepository extends JpaRepository<PublicDeckEntity, String> {
-}
--- clashroyaleback/src/main/java/egorteam/clashroyaleback/persistence/repository/UserPreferencesRepository.java
+++
@@ -1,7 +0,0 @@
-package egorteam.clashroyaleback.persistence.repository;
-
-import egorteam.clashroyaleback.persistence.entity.UserPreferenceEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPreferencesRepository extends JpaRepository<UserPreferenceEntity, String> {
-}
